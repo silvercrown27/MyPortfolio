a68a861b6264b27481b24417b7877ab6a965d356
silvercrown27 <bradabwao14@gmail.com>
2023-04-02 16:17:40 +0300

    my portfolio

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage, name='home'),
-    path('blog', views.BlogPage, name='blog'),
-    path('services', views.ServicesPage, name='services'),
-    path('contact', views.ContactPage, name='contact'),
-    path('portfolio', views.PortfolioPage, name='portfolio'),
+    path('blog/', views.BlogPage, name='blog'),
+    path('services/', views.ServicesPage, name='services'),
+    path('contact/', views.ContactPage, name='contact'),
+    path('portfolio/', views.PortfolioPage, name='portfolio'),
 ]
